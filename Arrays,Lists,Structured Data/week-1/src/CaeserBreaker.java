6a089f2da18d63307dabd13cbff0dadb19fff796
Sameer Miriyala <sameer.miriyala@zemosolabs.com>
2020-10-11 01:07:56 +0530

    added ceaser breaker

---
+++ Arrays,Lists,Structured Data/week-1/src/CaeserBreaker.java
@@ -0,0 +1,88 @@
+import edu.duke.*;
+
+public class CaeserBreaker {
+    public String decrypt(String encrypt) {
+        CaesarCipher cc = new CaesarCipher();
+        int[] freqs = countLetters(encrypt);
+        int maxDex = maxIndex(freqs);
+        int dkey = maxDex - 4;
+        if(maxDex < 4) {
+            dkey = 26 - (4 - maxDex);
+        }
+        System.out.println(dkey + " " + (26 - dkey));
+        return cc.encrypt(encrypt, 26 - dkey);
+    }
+    public int[] countLetters(String encrypt) {
+        int[] counts = new int[26];
+        for(int i = 0; i < encrypt.length(); i++) {
+            char ch = Character.toLowerCase(encrypt.charAt(i));
+            if(ch >= 'a' && ch <= 'z') {
+                counts[ch-'a']++;
+            }
+        }
+        return counts;
+    }
+
+    public int maxIndex(int[] freqency) {
+        int index = 0;
+        for(int i = 0; i < freqency.length; i++) {
+            if(freqency[i] > freqency[index]) {
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    String halfOfString(String message, int start) {
+        String halfString = "";
+        for (int i = start; i < message.length() ; i+= 2) {
+            halfString = halfString + message.charAt(i);
+        }
+        return halfString;
+    }
+
+    int getKey(String s) {
+        int[] freqs = countLetters(s);
+        int key = maxIndex(freqs);
+        return key;
+    }
+    void decryptTwoKeys(String encrypted) {
+        CaesarCipher cc = new CaesarCipher();
+        String firstHalf = halfOfString(encrypted, 0);
+        String secondHalf = halfOfString(encrypted, 1);
+        int key1 = getKey(firstHalf);
+        int key2 = getKey(secondHalf);
+        int dkey1 = key1 - 4;
+        int dkey2 = key2 - 4;
+        if(key1 < 4) {
+            dkey1 = 26 - (4 - key1);
+        }
+        if(key2 < 4) {
+            dkey2 = 26 - (4 - key2);
+        }
+        System.out.println("key1: " + dkey1 + " key2: " + dkey2);
+        System.out.println(cc.encryptTwoKeys(encrypted, 26 - dkey1, 26 - dkey2));
+    }
+
+    public void testDecrypt() {
+        FileResource fr = new FileResource();
+        System.out.println(decrypt(fr.asString()));
+    }
+
+    public void testHalfOfString() {
+        System.out.println(halfOfString("Qbkm Zgis", 0)); //Qk gs
+        System.out.println(halfOfString("Qbkm Zgis", 1)); //bmZi
+    }
+
+    public void testDecryptTwoKeys() {
+        FileResource fr = new FileResource();
+        String message = fr.asString();
+        //String message = "xyzaeeeee";
+        decryptTwoKeys(message);
+    }
+
+    public static void main(String[] args) {
+        CaeserBreaker obj = new CaeserBreaker();
+        obj.testDecryptTwoKeys();
+    }
+}
