e125de951c527569c01633e39764f3132d824666
Sameer Miriyala <sameer.miriyala@zemosolabs.com>
2020-10-16 17:59:38 +0530

    duplicate files deleted

--- Assignments/data/Data.java
+++
@@ -1,9 +0,0 @@
-package Assignments.data;
-
-public class Data {
-    int anInt;
-    char aChar;
-    public void print() {
-        System.out.println("int value: "+anInt+" char value: "+aChar);
-    }
-}
